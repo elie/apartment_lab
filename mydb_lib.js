a4c82c2849cefbff09bbcae6f9f77f3aaeb2de4c
wdi-sf-fall <markus.guehrs@generalassemb.ly>
2014-10-15 14:41:15 -0700

    added

---
+++ mydb_lib.js
@@ -0,0 +1,29 @@
+"use strict"
+
+var pg = require('pg');
+
+function DB(database, port, host) {
+  this.config = {
+    database: database,
+    port: port,
+    host: host
+  };
+}
+
+DB.prototype.connect = function(runAfterConnecting){
+  pg.connect(this.config, function(err, client, done){
+      if (err) {
+           console.error("OOOPS!!! SOMETHING WENT WRONG!", err);
+      }
+      runAfterConnecting(client);
+      done();
+  });
+};
+
+DB.prototype.query = function(statement, params, callback){
+  this.connect(function(client){
+    client.query(statement, params, callback);
+  });
+};
+
+module.exports = DB;
---
+++ superdry.js
@@ -0,0 +1,19 @@
+"use strict"
+
+var DB = require('./mydb_lib.js');
+
+var db = new DB("library_example_app", 5432, "localhost");
+
+db.query("SELECT * FROM books;", function(err, resultSet){
+    if (err) console.log("SELECT FAILED :-(", err);
+    console.log(resultSet.rows);
+});
+
+db.query("INSERT INTO books (title, author) VALUES ($1, $2) RETURNING *",
+["The Great Gatsby", "Fitzgerald"], function(err, resultSet){
+  if (err) console.log("INSERT FAILED :-(", err);
+  console.log(resultSet.rows);
+});
+
+
+
